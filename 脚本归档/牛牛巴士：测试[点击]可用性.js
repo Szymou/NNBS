e4af42127628281fba2b25fb109957035981a0ae
Administrator <1047697114@qq.com>
2023-09-04 07:40:54 +0800

    牛牛巴士：测试[点击]可用性--一键自动测试

--- 脚本归档/牛牛巴士：测试[点击]可用性.js
+++ 脚本归档/牛牛巴士：测试[点击]可用性.js
@@ -1,9 +1,9 @@
 auto.waitFor();
 app.launchApp("大麦");
 console.show();
-console.setPosition(100,100);
+console.setPosition(100, 100);
 var packageNameKk = "牛牛巴士.";
-console.setTitle(packageNameKk + "：点击测试","#ff11ee00",30);
+console.setTitle(packageNameKk + "：点击测试", "#ff11ee00", 30);
 console.setLogSize(10);
 console.setMaxLines(500);
 console.setBackgroud("#99000000");
@@ -15,91 +15,95 @@ console.setCanInput(false);
 
 //效果：如果执行脚本之后，能自动进入场次页，说明手机支持该种点击方式，记录好自己的mode即可
 
-//步骤1：预先打开dm的票详情页，也就是选择场次页的上一页。
-//步骤2：设置这里的mode，然后点击执行。
-    var mode = 1;//依次输入1 2 3 4 5
-    
-    
-    
+
+
 //下面不需要管
-    print("捕获票详情页中...");
-    print("\n请手动去到票详情页。");
-    var btnB = id("tv_left_main_text").findOne();
-    onClickMode(btnB, mode);
-    function onClickMode(btn, mode){
-        if(packageNameKk.length%3==2){print("\n模拟正常");}
-        //文本
-        var btnText = btn.text();
-        //坐标
-        var rectBtnBuy = btn.bounds();
-        var clickPosX = rectBtnBuy.centerX();
-        var clickPosY = rectBtnBuy.centerY();
-        
-        if(1 == mode){
-           print("\n这是xxx.click()点击，如果进入场次页，说明支持xxx.click()");
-           if(packageNameKk.length%2!=1 && packageNameKk.length !=5){
-                for(;;){
-                    print("\n脚本失效");
-                    sleep(300);
-                }            
-            }
-           btn.click();
-        }
-        
-       if(2 == mode){
-           print("\n这是click()点击，如果进入场次页，说明支持click()");
-           if(packageNameKk.length%2!=1 && packageNameKk.length !=5){
-                for(;;){
-                    print("\n脚本失效");
-                    sleep(300);
-                }            
-            }
-           click(btnText);
-       }
-        
-       if(3 == mode){
-           print("\n这是text点击，如果进入场次页，说明支持text");
-           if(packageNameKk.length%2!=1 && packageNameKk.length !=5){
-                for(;;){
-                    print("\n脚本失效");
-                    sleep(300);
-                }            
-            }
-           text(btnText).click();
-       }
-       
-       if(mode == 4){
-           print("\n这是click(x,y)点击，如果进入场次页，说明支持click(x,y)");
-           if(packageNameKk.length%2!=1 && packageNameKk.length !=5){
-                for(;;){
-                    print("\n脚本失效");
-                    sleep(300);
-                }            
-            }
-           click(clickPosX, clickPosY);
-       }
-       
-       if(mode == 5){
-           print("\n这是press点击，如果进入场次页，说明支持press");
-           if(packageNameKk.length%2!=1 && packageNameKk.length !=5){
-                for(;;){
-                    print("\n脚本失效");
-                    sleep(300);
-                }            
-            }
-           press(clickPosX, clickPosY, 20);
-       }
-       
-        
-        //print(currentActivity() );
-        //print(currentPackage());
-        print("\n静等结果，手别碰屏幕，否则不准确");
-        sleep(1000);
-     //  cn.damai
-      // cn.damai.commonbusiness.seatbiz.sku.qilin.ui.NcovSkuActivity
-       if(currentActivity() == "cn.damai.commonbusiness.seatbiz.sku.qilin.ui.NcovSkuActivity" ){
-           print("\n成功进去场次页面！你选择的模式是：" + mode);
-       }else{
-           print("\n进去失败，不支持模式：" + mode);
-       }
+print("捕获票详情页中...");
+print("\n请手动去到票详情页。");
+var btnB = id("tv_left_main_text").findOne();
+for (i = 0; i < 5; i++) {
+    print("\n\n=========模式" + (i + 1) + "开始=========");
+    onClickMode(btnB, i + 1);
+    print("=========模式" + (i + 1) + "结束=========");
+}
+
+function onClickMode(btn, mode) {
+    if (packageNameKk.length % 3 == 2) {
+        print("\n模拟正常");
     }
+    //文本
+    var btnText = btn.text();
+    //坐标
+    var rectBtnBuy = btn.bounds();
+    var clickPosX = rectBtnBuy.centerX();
+    var clickPosY = rectBtnBuy.centerY();
+
+    if (1 == mode) {
+        print("\n这是xxx.click()点击，如果进入场次页，说明支持xxx.click()");
+        if (packageNameKk.length % 2 != 1 && packageNameKk.length != 5) {
+            for (; ;) {
+                print("\n脚本失效");
+                sleep(300);
+            }
+        }
+        btn.click();
+    }
+
+    if (2 == mode) {
+        print("\n这是click()点击，如果进入场次页，说明支持click()");
+        if (packageNameKk.length % 2 != 1 && packageNameKk.length != 5) {
+            for (; ;) {
+                print("\n脚本失效");
+                sleep(300);
+            }
+        }
+        click(btnText);
+    }
+
+    if (3 == mode) {
+        print("\n这是text点击，如果进入场次页，说明支持text");
+        if (packageNameKk.length % 2 != 1 && packageNameKk.length != 5) {
+            for (; ;) {
+                print("\n脚本失效");
+                sleep(300);
+            }
+        }
+        text(btnText).click();
+    }
+
+    if (mode == 4) {
+        print("\n这是click(x,y)点击，如果进入场次页，说明支持click(x,y)");
+        if (packageNameKk.length % 2 != 1 && packageNameKk.length != 5) {
+            for (; ;) {
+                print("\n脚本失效");
+                sleep(300);
+            }
+        }
+        click(clickPosX, clickPosY);
+    }
+
+    if (mode == 5) {
+        print("\n这是press点击，如果进入场次页，说明支持press");
+        if (packageNameKk.length % 2 != 1 && packageNameKk.length != 5) {
+            for (; ;) {
+                print("\n脚本失效");
+                sleep(300);
+            }
+        }
+        press(clickPosX, clickPosY, 20);
+    }
+
+
+    //print(currentActivity() );
+    //print(currentPackage());
+    print("\n静等结果，手别碰屏幕，否则不准确");
+    sleep(1000);
+    //  cn.damai
+    // cn.damai.commonbusiness.seatbiz.sku.qilin.ui.NcovSkuActivity
+    if (currentActivity() == "cn.damai.commonbusiness.seatbiz.sku.qilin.ui.NcovSkuActivity") {
+        print("\n结果：：：进去场次页面[成功]!!!支持模式：" + mode);
+        back();
+    } else {
+        print("\n结果：：：进去场次页面[失败]!!!不支持模式：" + mode);
+    }
+}
